5c13ea0dbeaf577887a9b0057ad913dcfe7af5dc
Mikhail Hyde <hyde.mikhail@gmail.com>
2024-04-08 21:02:22 -0400

    plot both channels

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ def main():
     left = [d.left for d in data]
     right = [d.right for d in data]
 
-    plt.plot(left)
-    plt.ylim(-15_000, 15_000)
+    _, axs = plt.subplots(2, 1, figsize=(10, 4))
+
+    axs[0].plot(left)
+    axs[0].set_title('Left')
+
+    axs[1].plot(right)
+    axs[1].set_title('Right')
+
+    axs[0].set_ylim(-15_000, 15_000)
+    axs[1].set_ylim(-15_000, 15_000)
+    plt.tight_layout()
     plt.show()
 
 
