e70b224fd36d4e9c3febc39999e3c26d535e4979
Mikhail Hyde <hyde.mikhail@gmail.com>
2024-04-09 18:59:56 -0400

    pipfile and setup.py nonsense

--- lib/setup.py
+++ lib/setup.py
@@ -1,44 +1,49 @@
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
 import os
 import sys
 import subprocess
-from setuptools import setup, find_packages, Extension
-from setuptools.command.build_ext import build_ext
-
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
-
 class CMakeBuild(build_ext):
     def run(self):
+        try:
+            subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+                      "-DPYTHON_EXECUTABLE=" + sys.executable]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
 
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
+        build_temp = os.path.join(os.getcwd(), "build", ext.name)
 
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
+        if not os.path.exists(build_temp):
+            os.makedirs(build_temp)
 
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=build_temp)
+        subprocess.check_call(["cmake", "--build", ".", "--target", ext.name] + build_args, cwd=build_temp)
 
 setup(
-    name='musik-box',
-    version='0.0.1',
-    author='Mikhail Hyde',
-    description='',
-    long_description='',
-    ext_modules=[CMakeExtension('musik-box')],
+    name="musik_box",
+    version="0.0.1",
+    author="Your Name",
+    description="A Python package with C++ extensions",
+    long_description="",
+    packages=find_packages(),
+    ext_modules=[CMakeExtension("musik_box", "audio")],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
-    packages=find_packages(),
 )
