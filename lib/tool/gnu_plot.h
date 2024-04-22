1131057a727dddfc2e833ad6bd910e396c418dbb
mhydesf <hyde.mikhail@gmail.com>
2024-04-22 19:20:25 -0400

    op overloading

--- lib/tool/gnu_plot.h
+++ lib/tool/gnu_plot.h
@@ -2,15 +2,16 @@
 
 #include <vector>
 #include <fmt/format.h>
+#include <algorithm>
 #include "gnuplot-iostream.h"
 
 namespace MusikBox::Tools {
 
-template <typename T>
 class Plotter {
 public:
     Plotter() {}
 
+    template <typename T>
     void Plot1D(const std::vector<T>& data) {
         gp << fmt::format(
             "set xrange [0:{}]\nset yrange [{}:{}]\n",
@@ -20,6 +21,15 @@ public:
         );
         gp << "plot '-'\n";
         gp.send1d(data);
+    }
+
+    void Plot1D(const std::vector<std::complex<double>>& data) {
+        std::vector<int16_t> data_i;
+        data_i.resize(data.size());
+        for (size_t i = 0; i < data.size(); i ++) {
+            data_i[i] = static_cast<int16_t>(data[i].real());
+        }
+        Plot1D<int16_t>(data_i);
     }
 
 private:
