f4b20eb15c806c7fcefd34149b6d72607c866434
mhydesf <hyde.mikhail@gmail.com>
2024-04-22 23:30:03 -0400

    plot std::pair

--- lib/tool/gnu_plot.h
+++ lib/tool/gnu_plot.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <cstdint>
-#include <vector>
 #include <format>
+#include <vector>
+#include <cstdint>
 #include <algorithm>
 #include <gnuplot-iostream.h>
 
@@ -12,25 +12,38 @@ class Plotter {
 public:
     Plotter() {}
 
-    template <typename T>
-    void Plot1D(const std::vector<T>& data) {
-        gp << std::format(
-            "set xrange [0:{}]\nset yrange [{}:{}]\n",
-            data.size(),
-            *std::max_element(data.begin(), data.end()),
-            *std::min_element(data.begin(), data.end())
-        );
-        gp << "plot '-'\n";
-        gp.send1d(data);
+    template <typename T1, typename T2>
+    void Plot1D(const std::vector<std::pair<T1, T2>>& data) {
+        auto maxIt = std::max_element(data.begin(), data.end(),
+            [](const std::pair<T1, T2>& a, const std::pair<T1, T2>& b) {
+                return a.second < b.second;
+            });
+
+        auto minIt = std::min_element(data.begin(), data.end(),
+            [](const std::pair<T1, T2>& a, const std::pair<T1, T2>& b) {
+                return a.second < b.second;
+            });
+
+        if (maxIt != data.end() && minIt != data.end()) {
+            std::string command = std::format("set xrange [0:{}]\nset yrange [{:.2f}:{:.2f}]\n",
+                                              data.size(),
+                                              static_cast<float>(minIt->second),
+                                              static_cast<float>(maxIt->second));
+            gp << command;
+            gp << "plot '-'\n";
+            gp.send1d(data);
+        }
     }
 
-    void Plot1D(const std::vector<std::complex<double>>& data) {
-        std::vector<int16_t> data_i;
+    void Plot1D(const std::vector<std::pair<float, std::complex<double>>>& data) {
+        std::vector<std::pair<float, int16_t>> data_i;
         data_i.resize(data.size());
         for (size_t i = 0; i < data.size(); i++) {
-            data_i[i] = static_cast<int16_t>(data[i].real());
+            float x = data[i].first;
+            int16_t y = static_cast<int16_t>(data[i].second.real());
+            data_i[i] = std::pair(x, y);
         }
-        Plot1D<int16_t>(data_i);
+        Plot1D<float, int16_t>(data_i);
     }
 
 private:
