5f645df926d76d0a60e81297e2ae0e5ab4e21256
mhydesf <hyde.mikhail@gmail.com>
2024-05-09 08:23:43 -0400

    test fftmanager

--- test/test_fft.cpp
+++ test/test_fft.cpp
@@ -1,40 +1,28 @@
+#include <optional>
 #include <vector>
 #include <fft.h>
 #include <wav.h>
 #include <gnu_plot.h>
 
-std::vector<float> generateSinWave(float amplitude,
-                                   float frequency,
-                                   uint32_t sample_rate,
-                                   float duration) {
-    std::vector<float> ret;
-    int totalSamples = static_cast<int>(sample_rate * duration);
-    for (int n = 0; n < totalSamples; ++n) {
-        double value = amplitude * sin(n * 2 * M_PI * frequency / sample_rate);
-        ret.push_back(value);
+int main() {
+    // Signal
+    const uint32_t sample_rate = 44100;
+    float frequency = 40.0;
+    std::vector<float> wave;
+    wave.resize(sample_rate);
+    for (int i = 0; i < sample_rate; ++i) {
+        wave[i] = 1.0 * sin(2.0 * M_PI * frequency * i / sample_rate);
     }
 
-    return ret;
-}
-
-int main() {
-    const uint32_t sample_rate = 44100;
-    const float freq = 10.0;
-    auto wave = generateSinWave(1.0, freq, sample_rate, 1000.0);
+    // FFT Test
     auto wave_d = MusikBox::Audio::WAV::convertAudioData<float, double>(wave);
-
     auto fft = MusikBox::DSP::FFTManager();
     auto result = fft.computeFFT(wave_d);
-    fft.reduceFFT(result, sample_rate, 10*freq);
+    auto pl = fft.convertToPairVector(result, sample_rate, 100);
 
-    std::vector<std::pair<float, std::complex<double>>> pl(result.size());
-    for (size_t i = 0; i < result.size(); i++) {
-        float x = i * fft.getFreqencyStep(sample_rate, result.size());
-        std::complex<double> y = result[i];
-        pl[i] = std::pair(x, y);
-    }
-    
+    // Plot
     MusikBox::Tools::Plotter plot;
+    // plot.SetBounds(0, 100, std::nullopt, std::nullopt);
     plot.Plot1D(pl);
 
     return 0;
